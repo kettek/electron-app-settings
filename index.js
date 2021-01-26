22f67f64ac88e275d674ae0478d649b8f54a278f
kts of kettek (nyaa) <kettek1@kettek.net>
2021-01-26 00:29:41 -0800

    Switch to MPL-2.0; bump version to 1.1.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  *
  * @module index
  * @author Ketchetwahmeegwun T. Southall / kts of kettek
- * @copyright 2018 Ketchetwahmeegwun T. Southall <kettek1@kettek.net>
- * @license GPL-3.0
+ * @copyright 2018-2021 Ketchetwahmeegwun T. Southall <kettek1@kettek.net>
+ * @license MPL-2.0
  */
 module.exports = (process.type === 'browser' ? require('./lib/Main') : require('./lib/Renderer'));
